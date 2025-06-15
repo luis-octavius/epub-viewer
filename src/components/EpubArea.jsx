d077c21d4e8bbbd185324396b8d4b64272bcc4c4
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-14 22:26:44 -0300

    feat: render ebook section

--- src/components/EpubArea.jsx
+++ src/components/EpubArea.jsx
@@ -1,14 +1,57 @@
 import { useState, useRef, useEffect } from "react";
+import ePub from "epubjs";
 
 export default function EpubArea() {
+    const [isFileUpload, setIsFileUpload] = useState(false);
+    const [book, setBook] = useState(null);
+
+    const getFile = () => {
+        const fileUpload = document.getElementById("file").files[0];
+        setIsFileUpload(true);
+        let book = ePub(fileUpload);
+        console.log(book);
+        setBook(book);
+    }
+
+    const loadEbook = () => {
+        const rendition = book.renderTo("area", {width: "100%", height: 400});
+        console.log("Book: ", book);
+        console.log("Rendition: ", rendition)
+        rendition.display();
+    }
+
     return (
-        <div className="epub pl-5 border-2 bg-[var(--main-bg)] border-black">
-            <input 
-                type="file"
-            />
-            <button
-                type="submit"
-            />
+        <div className="flex flex-col items-center justify-center epub pl-5 bg-[var(--main-bg)] ">
+            {!isFileUpload ? (
+                <form 
+                method="post"
+                className="flex flex-col items-center">
+                <label 
+                    htmlFor="file"
+                    className="button"
+                >Choose Epub file to upload</label>
+                <input 
+                    name="file"
+                    id="file"
+                    type="file"
+                    accept=".epub"
+                    onChange={()=> getFile()}
+                    />
+                <button
+                    className="w-15 bg-white rounded-md border-1 border-black p-1 m-1"
+                    type="submit"
+                >
+                Send 
+                </button>
+                </form>
+            ) : (
+                <div
+                    id="area"
+                    onLoad={loadEbook()} 
+                    className="w-full h-full"   
+                >
+                </div>
+            )}
         </div>
     )
 }

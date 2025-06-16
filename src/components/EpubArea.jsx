c66ff1bb004d5c2ec92758d9f7f12413ae2ee4bb
Luis Octávio <luisoctavius.sc@gmail.com>
2025-06-15 22:47:21 -0300

    new edits

--- src/components/EpubArea.jsx
+++ src/components/EpubArea.jsx
@@ -4,33 +4,20 @@ import ePub from "epubjs";
 export default function EpubArea() {
     const [isFileUpload, setIsFileUpload] = useState(false);
     const [book, setBook] = useState(null);
-    const [rendition, setRendition] = useState(null);
-    const [displayed, setDisplayed] = useState(null);
+    const viewerRef = useRef(null);
 
     const getFile = () => {
         const fileUpload = document.getElementById("file").files[0];
         setIsFileUpload(true);
-        let book = ePub(fileUpload);
-        setBook(book);
+        let actualBook = ePub(fileUpload);
+        setBook(actualBook);
     }
 
     const loadEbook = () => {
-        const useRendition = book.renderTo("area", {
-            method: "paginate", 
-            width: "100%", 
-            height: "100%",
-            manager: "continuous"
-        });
-        setRendition(useRendition);
-        console.log("Book: ", book);
-        console.log("Rendition: ", useRendition);
-    }
-
-    const renderBook = () => {
+        const rendition = book.renderTo("area", { method:'paginate', width: '100%', height: '100%'})
+        viewerRef.current = rendition;
         rendition.display();
     }
-
-
     return (
         <div className="flex flex-col items-center justify-center epub bg-[var(--main-bg)]">
             {!isFileUpload ? (
@@ -48,21 +35,19 @@ export default function EpubArea() {
                     accept=".epub"
                     onChange={()=> getFile()}
                     />
-                <button
+                {/* <button
                     className="w-15 bg-white rounded-md border-1 border-black p-1 m-1"
                     type="submit"
                 >
                 Send 
-                </button>
+                </button> */}
                 </form>
             ) : (
                 <div
                     id="area"
-                    onLoad={() => {
-                        loadEbook();
-                        renderBook();
-                    }}
-                    className="w-full h-full"   
+                    ref={viewerRef}
+                    onLoad={loadEbook()}
+                    className="w-full h-full flex flex-col justify-center items-center"   
                 >
                 <div
                     id="navigation"

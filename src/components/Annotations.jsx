debc7091a2d2f94ceda3f4a2849c81a419a4123e
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-19 11:43:23 -0300

    feat: add router to handle the routes

---
+++ src/components/Annotations.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Modal from './Modal';
+
+export default function Annotations() {
+    const [notes, setNotes] = useState(["Teste", "Segundo Teste"]);
+
+    const createNote = () => {
+        
+    }
+
+    return (
+        <div>
+            <button
+                onClick={createNote}
+            >
+                Adicionar Nota
+            </button>
+            <ul>
+                {notes.map((note) => {
+                    <li>{note}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Epub from './components/EpubArea';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(

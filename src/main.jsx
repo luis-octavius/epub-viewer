9a39d2c8265e3a65161aae53276f2034958fa028
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-19 17:37:44 -0300

    feat: add router to handle the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import App from './App.jsx'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { 
+    createBrowserRouter,
+    RouterProvider,
+} from 'react-router'
 import Epub from './components/EpubArea';
 import './index.css'
+import Annotations from './components/Annotations';
+import Home from './components/Home';
+import Content from './components/Content';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            { path: "home", element: <Home /> },
+            { path: "content", element: <Content /> },
+            { path: "annotations", element: <Annotations /> },
+            { path: "epub", element: <Epub /> }
+        ],
+    },
+])
+
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router} />
 )

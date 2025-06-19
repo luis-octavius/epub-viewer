e9cb71b03e3e0f55cda77cdc9582539efc3c9f13
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-19 17:35:02 -0300

    feat: routes to navigate between the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './index.css'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import EpubArea from './components/EpubArea';
+import Home from './components/Home';
 
 function App() {
   return (
       <>
         <Header />
         <Navbar />
-        <EpubArea />
+        <Home />
         <Footer />
       </>
       )

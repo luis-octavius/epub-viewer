83b3a73256710274c60a2cca290fbce4746ff6d1
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-19 17:37:25 -0300

    feat: links to routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router';
+
 export default function Navbar() {
   return (
-    <div className="navbar flex flex-col bg-[var(--sidebar-color)]">
-      <h3 className="h3-link">Home</h3> 
-      <h3 className="h3-link">Annotations</h3>     
+    <div className="navbar flex flex-col bg-[var(--sidebar-color)] shadow-[4px_0_6px_-1px_rgba(0,0,0,0.5)]">
+      <Link to="/content" className="h3-link">Home</Link> 
+      <Link to="/epub" className="h3-link">Epub</Link>
+      <Link to="/annotations" className="h3-link">Annotations</Link>     
     </div>
   )
 }

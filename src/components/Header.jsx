d7280e3a7aef9c1e5c062fe1d2c8f01f89ff452d
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-19 17:37:10 -0300

    style: adjust ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
     return (
-        <div className="header text-4xl my-auto py-2 bg-[var(--header-color)] w-full h-full flex">
+        <div className="shadow-[0_4px_6px_-1px_rgba(0,0,0,0.5)] header text-4xl my-auto py-2 bg-[var(--header-color)] w-full h-full flex">
             <h1 className="my-auto pl-5 font-[var(--base-font)] text-amber-950">EPUB Viewer</h1>
         </div>
     )

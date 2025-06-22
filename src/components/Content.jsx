b523468476e60b8c00caad0bdab0766917fb1d55
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-22 16:09:52 -0300

    feat: api fetching to show ebooks in home page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
+
 export default function Content() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://openlibrary.org/search.json?title=laws&lang=pt&author=plato', {
+      mode: "cors",
+    })
+    .then((response) => { 
+      response.json();
+      console.log(response)
+    })
+    .catch((error) => console.error(error));
+  }, [])
+
   return (
     <div className="justify-self-center m-auto">
-      <h1 className="text-3xl font-bold text-[var(--main-bg)]">
-        WELCOME TO EPUB VIEWER!
-      </h1>
+
     </div>
   );
 }

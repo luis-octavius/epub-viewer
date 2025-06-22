6a0ec558b9b693fe31136a61fec2eb4ea3c8124d
Big-Plato <luisoctavius.sc@gmail.com>
2025-06-22 16:09:31 -0300

    feat: render the notes and styling the modal

--- src/components/Annotations.jsx
+++ src/components/Annotations.jsx
@@ -1,14 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Form } from "react-router";
 import Modal from "./Modal";
+import axios from 'axios';
 
 export default function Annotations() {
-  const [notes, setNotes] = useState(["Teste", "Segundo Teste"]);
+  const [notes, setNotes] = useState([]);
+  const [modal, setModal] = useState(false);
 
-  const createNote = () => {};
+  useEffect(() => {
+    axios.get('http://localhost:3000/notes')
+      .then((response) => {
+        setNotes(response.data);
+      })
+      .catch((error) => {
+        console.error('Houve um erro de conexão: ', error)
+      })
+  }, []);
 
   return (
-    <div>
-      <button onClick={createNote}>Adicionar Nota</button>
+    <div className="flex flex-col items-center justify-center">
+      <button
+        className="rounded-md bg-[var(--sidebar-color)] p-2 cursor-pointer"
+        onClick={() => setModal(true)}
+      >Adicionar Nota</button>
+      <Modal
+        openModal={modal}
+        closeModal={() => setModal(false)}
+      >
+        <Form 
+          className="flex flex-col"
+          action="/notes" method="post">
+          <label className="block" htmlFor="title">Title
+          </label>
+            <input name="title" placeholder="Note's name" type="text"></input>
+
+          <label htmlFor="note">Note
+          </label>
+            <textarea name="note" rows="5" cols="30" type="text" placeholder="Note's text"></textarea>
+
+          <button className="button" type="submit">Send</button>
+        </Form>
+      </Modal>
       <ul>
         {notes.map((note) => {
           <li>{note}</li>;
